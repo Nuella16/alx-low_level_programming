62dcfcf16d2bb046f893c73b51b37101c5edc8bb
Nuella16 <nanaessilfu18@gmail.com>
2023-08-02 14:30:19 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,64 +1,46 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - returns the nth node of a linked list
- * @head: pointer to the head of the list
- * @idx: index of the node to be added
- * @n: content of the new node
+ *insert_nodeint_at_index-Inserts new node in linked list at specific position
+ * @head: A pointer to the first node of the list
  *
- * Return: the address of the node
+ *  @n: The data to insert into the new node
+ *
+ *@idx:Is the index for the new node to be added
+ *
+ * Return:Returns a pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = NULL;
-	listint_t *previous_node = NULL;
-	unsigned int i = 0;
+	unsigned int a;
+	listint_t *new;
+	listint_t *temporary = *head;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL || idx > listint_len(*head))
-	{
-		free(new_node);
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-	while (head != NULL)
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
 	{
-		if (i == idx)
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	for (a = 0; temporary && a < idx; a++)
+	{
+		if (a == idx - 1)
 		{
-			if (i == 0)
-			{
-				new_node->next = *head;
-				*head = new_node;
-				return (new_node);
-			}
-			new_node->next = previous_node->next;
-			previous_node->next = new_node;
-			return (new_node);
+			new->next = temporary->next;
+			temporary->next = new;
+			return (new);
 		}
-		else if ((i + 1) == idx)
-			previous_node = *head;
-		head = &((*head)->next);
-		i++;
+		else
+			temporary = temporary->next;
 	}
+
 	return (NULL);
-}
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
 }
